5989e65414d146c183b6069eb429ad7272e2f24c
griffin11 <92304341+griffin11@users.noreply.github.com>
2022-01-11 10:45:14 +0100

    h

--- testtemp.py
+++ testtemp.py
@@ -2,15 +2,13 @@ from gpiozero import MCP3008
 from time import sleep
 import math
 
-
-
 tmp   = MCP3008(channel=2, device=0)
+
 while True: 
   adc   = (tmp.value *1000)
-  adc_1 = (((adc * 5.0 / 1024) -0.5) / 0.01)
+  adc_1   = (((adc * 3.3 / 1024) -0.5) / 0.01)
+  num = round(adc_1, 2)
+  print ( num)
   
-  ad    = (tmp.value)
-  print (adc_1)
   sleep(2)
   
- 
