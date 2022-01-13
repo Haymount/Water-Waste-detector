ff4b15e2386744396f7eb338ecba249bf4a33379
aringg20 <73231673+aringg20@users.noreply.github.com>
2022-01-14 00:59:45 +0100

    fixed if statements
    mangler kun at få rum temperaturen til at blive målt med et større tidsinterval

--- wateralarm.py
+++ wateralarm.py
@@ -10,8 +10,7 @@ GPIO.setmode(GPIO.BCM)                                #Opsætning af RPi.GPIO
 GPIO14 = 13                                           #Pin nummer buzzer
 GPIO26 = 26                                           #Pin nummer rød-led
 GPIO19 = 19                                           #Pin nummer grøn-led
-GPIO.setup([GPIO26, GPIO14, GPIO19], GPIO.OUT, initial=GPIO.LOW)
-#GPIO.setup(GPIO14, GPIO.OUT, initial=GPIO.LOW)        #Sætter pin som output, og starter alarmerne slukket
+GPIO.setup([GPIO26, GPIO14, GPIO19], GPIO.OUT, initial=GPIO.LOW)        #Sætter pin som output, og starter alarmerne slukket
 GPIO.setwarnings(False)
 tmp_1 = MCP3008(channel=1, device=0)                  # ADC output
 tmp_2 = MCP3008(channel=2, device=0)                  # ADC output
@@ -24,55 +23,54 @@ class main:                                           #Class for at bruge flere 
            # while True:
              #schedule.run_pending()
             # time.sleep(60)
-           
+    
             
         now = datetime.datetime.now()
     
-        adc_2v   = (tmp_2.value *1000)                   # Temp på rummet
-        adc_2r   = (((adc_2v * 3.3 / 1024) -0.5) / 0.01) # Ganger adc værdi med 1000
-        num_2    = round(adc_2r, 2)  
+        adc_2v = (tmp_2.value *1000)                   # Temp på rummet
+        adc_2r = (((adc_2v * 3.3 / 1024) -0.5) / 0.01) # Ganger adc værdi med 1000
+        num_2 = round(adc_2r, 2)  
+        num_2 = int(num_2)
 
-    
-        while True:
-            if roomtemp is not None:
-                roomtemp = num_2
-                print(roomtemp, (now.strftime("%d-%m-%Y %H:%M:%S")))
-                return num_2
-        
-                
-        
+        if roomtemp is not None:
+            roomtemp = num_2
+            print(("Room Temp: ", roomtemp), (now.strftime("%d-%m-%Y %H:%M:%S")))
+            return num_2
            
             
-
        
     def checktemp(a):                                  #Funktionen med alt indmaden
             watertemp = 0                              #Giver watertemp en start værdi
             roomtemp = a
             
                                                              # temp på rør 
-            adc_1v   = (tmp_1.value *1000)                   # Ganger adc værdi med 1000
-            adc_1r   = (((adc_1v * 3.3 / 1024) -0.5) / 0.01) # Ud regner vandtemp
-            num_1    = round(adc_1r, 2)                      # Laver så vi kun får 2 decimaler
+            adc_1v = (tmp_1.value *1000)                   # Ganger adc værdi med 1000
+            adc_1r = (((adc_1v * 3.3 / 1024) -0.5) / 0.01) # Ud regner vandtemp
+            num_1 = round(adc_1r, 2)                      # Laver så vi kun får 2 decimaler
+            num_1 = int(num_1)
             now = datetime.datetime.now()
+            
+            GPIO.output(GPIO19, True)
             
             sleep(2)
             if watertemp is not None:
                 watertemp = num_1                            #watertemp værdien bliver erstattet af temperaturmålerens aflæsnings-værdi
+                
     
-                print(watertemp, (now.strftime("%d-%m-%Y %H:%M:%S")))
-                if watertemp > 28:                           #Hvis temperaturen er OVER angivet værdi, så går alarmen af
+                print(("Water Temp: ", watertemp), (now.strftime("%d-%m-%Y %H:%M:%S")))
+                if watertemp > 30:                           #Hvis temperaturen er OVER angivet værdi, så går alarmen af
                     GPIO.output(GPIO14, True)
                     GPIO.output(GPIO26, True)
-                elif watertemp < 20:                         #Hvis temperaturen er UNDER angivet værdi, så går alarmen af
+                elif watertemp < 22:                         #Hvis temperaturen er UNDER angivet værdi, så går alarmen af
                     GPIO.output(GPIO14, True)
                     GPIO.output(GPIO26, True)
-                elif watertemp == roomtemp:
+                elif abs(watertemp - roomtemp) <= 3:
                     GPIO.output(GPIO14, False)
                     GPIO.output(GPIO26, False)
                 elif watertemp > roomtemp:
                     GPIO.output(GPIO14, True)
                     GPIO.output(GPIO26, True)
-                elif watertemp < roomtemp:
+                elif watertemp < roomtemp: 
                     GPIO.output(GPIO14, True)
                     GPIO.output(GPIO26, True)
                 else:                                        #Hvis temperaturen er i sikker range, er alarmerne slukket
@@ -82,9 +80,12 @@ class main:                                           #Class for at bruge flere 
                 print("No temp")
                 GPIO.cleanup()
 
-        
 
 while True:                                                  #While loop, for at class'en bliver kaldt igen og igen, så længe programmet kører
   main.checkroomtemp()
   main.checktemp(main.checkroomtemp()) 
   
+
+
+
+
